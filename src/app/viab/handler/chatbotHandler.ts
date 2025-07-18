1235a2a2b90d08f9141818be7ca19361752647a3
waleed wahab <walees,wahab321@gmail.com>
2025-07-18 23:52:29 +0500

    changes

--- src/app/viab/handler/chatbotHandler.ts
+++ src/app/viab/handler/chatbotHandler.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-const baseURL = process.env.NEXT_PUBLIC_AI_BASE_URL;
+// const baseURL = process.env.NEXT_PUBLIC_AI_BASE_URL;
 export const interviewHandler = async (values: any) => {
   try {
-    const resp = await axios.post(`${baseURL}/api/interview`, values);
+    const resp = await axios.post("https://kapcjtnj7e6cfthe5k22cqylki.srv.us/api/interview", values);
       
     return { message: resp?.data?.content, status: resp.status };
   } catch (error) {
@@ -12,7 +12,7 @@ export const interviewHandler = async (values: any) => {
 
 export const analyzeHandler = async (values: any) => {
   try {
-    const resp = await axios.post(`${baseURL}/api/analyze`, values);
+    const resp = await axios.post("https://kapcjtnj7e6cfthe5k22cqylki.srv.us/api/analyze", values);
       return { message: resp?.data?.content, status: resp.status };
   } catch (error) {
     console.log(`error: ${JSON.stringify(error)}`);
